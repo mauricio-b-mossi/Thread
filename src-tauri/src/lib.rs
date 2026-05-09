e7c1ea649e18eaf388a2d898166c3cc5c49bb9bf
mauricio-b-mossi <mauricio.b.mossi@gmail.com>
2026-05-09 15:24:46 -0400

    TASK-0002: Add SQLite persistence layer

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,11 @@
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+            persistence::open_app_database(&app_handle)?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running Thread");
 }
+pub mod persistence;
